0e32db51bbcc328e5f00c4bf89f8cf6d8a8265a3
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-10-09 22:39:19 +0100

    Refactor getStats and fetchLastYearMatchIds functions for improved match data handling; update storage path and enhance error handling

--- src/services/stats.js
+++ src/services/stats.js
@@ -1,34 +1,57 @@
+import { fa, tr } from "zod/locales";
 import {
   getMatchsByPUUID,
   getPUUIDByRiotID,
   getMatchTimeStamp,
+  getMatchData
 } from "./player.js";
 import fs from "fs";
 
 //remove when done
-const storagePath = ["C:/Users/AURES/Documents/storage.json", "C:/Users/admin/Documents/storage.json"];
+const storagePath = ["C:/Users/AURES/Documents/Hackthon-Storage/storage.json", "C:/Users/admin/Documents/storage.json"];
 
-function getStats(player) {
+async function getStats(player) {
   const puuid = getPUUIDByRiotID(player);
   if(fetchLastYearMatchIds(puuid, player.region)) {
-    const storageObj = JSON.parse(fs.readFileSync(storagePath[1], "utf-8"));
-    const matchesIDs = storageObj.yearMatchIDs
+    const storageObj = JSON.parse(fs.readFileSync(storagePath[0], "utf-8"));
+    const matchesIDs = storageObj[puuid].yearMatchIDs;
+    if(!storageObj[puuid].mathData) {
+      console.log("match data already saved");
+      return true;
+    }
+
+    for(let i of matchesIDs) {
+      const matchData = await getMatchData(i,player.region);
+      storageObj[puuid].matchData = matchData;
+    }
+
     //maybe use MONGO DB for this
     //way to check if we already fetched for this player (json file each as temp sol)
     //go throught each match data
     //save in json
-    // caluclate analytics
+    // caluclate analytics -> what do mean by analytics we need to defi
+    //players stats
+    //longest/shortest games
+    //number of games played in the least year
+    //number of games played for each game mode
+
   }
 }
 
 export async function fetchLastYearMatchIds(puuid, region) {
+  let requestCount = 5;
   let endTime = Math.floor(Date.now() / 1000);
   const yearInSec = 365 * 24 * 60 * 60;
   const startTime = endTime - yearInSec;
-  const requestCount = 5;
 
-  const storageObj = JSON.parse(fs.readFileSync(storagePath[1], "utf-8"));
+  const storageObj = JSON.parse(fs.readFileSync(storagePath[0], "utf-8"));
+  if (storageObj[puuid]) {
+    console.log("the player matchids already saved");
+    return true; 
+  }
 
+  storageObj[puuid] = { yearMatchIDs: [] };
+  
   while (endTime > startTime) {
     try { 
       const params = {
@@ -39,22 +62,30 @@ export async function fetchLastYearMatchIds(puuid, region) {
 
       const matchIDs = await getMatchsByPUUID(puuid, region, params);
 
-      if (!matchIDs || matchIDs.length === 0) break;
+      if (!matchIDs || matchIDs.length === 0) {
+        return true;
+      };
       console.log(
         `Fetched ${matchIDs.length} matches, total: ${storageObj.yearMatchIDs.length}`
       );
       //save matchIds
-      storageObj.yearMatchIDs.push(...matchIDs);
-      endTime = await getMatchTimeStamp(matchIDs[matchIDs.length - 1],region) - 1;
-      fs.writeFileSync(storagePath[1], JSON.stringify(storageObj, null, 2));
+      storageObj[puuid].yearMatchIDs.push(...matchIDs);
+      fs.writeFileSync(storagePath[0], JSON.stringify(storageObj, null, 2));
+
+      oldestMatchTime = await getMatchTimeStamp(matchIDs[matchIDs.length - 1],region);
+      endTime = (oldestMatchTime /1000) - 1;
+
       await new Promise((res) => setTimeout(res, 1000));
     } catch (error) {
       console.error("Error fetching matches:", error.message);
-      await new Promise((res) => setTimeout(res, 5000));
-      requestCount-=1;
-      if(requestCount == 0 ) {
-        return true;
+      requestCount--;
+
+      if(requestCount <= 0 ) {
+        console.error("Max retries reached, aborting.");
+        return false;
       }
+      console.log(`Retrying... (${remainingRetries} retries left)`);
+      await new Promise((res) => setTimeout(res, 5000));
     }
   }
   return true;
