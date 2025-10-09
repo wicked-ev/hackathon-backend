15b5b9792f5ba2d3355c1eeda2cf34dbfa1729f3
wicked <youcefzeith@gmail.com>
2025-10-09 16:53:52 +0100

    Refactor fetchLastYearMatchIds to improve time calculations and storage handling; update getStats to utilize fetched match IDs

--- src/services/stats.js
+++ src/services/stats.js
@@ -5,21 +5,32 @@ import {
 } from "./player.js";
 import fs from "fs";
 
+//remove when done
+const storagePath = ["C:/Users/AURES/Documents/storage.json", "C:/Users/admin/Documents/storage.json"];
+
 function getStats(player) {
   const puuid = getPUUIDByRiotID(player);
+  if(fetchLastYearMatchIds(puuid, player.region)) {
+    const storageObj = JSON.parse(fs.readFileSync(storagePath[1], "utf-8"));
+    const matchesIDs = storageObj.yearMatchIDs
+    //maybe use MONGO DB for this
+    //way to check if we already fetched for this player (json file each as temp sol)
+    //go throught each match data
+    //save in json
+    // caluclate analytics
+  }
 }
 
 export async function fetchLastYearMatchIds(puuid, region) {
-  let endTime = Date.now() / 1000;
-  const yearInMiliSec = 365 * 24 * 60 * 60;
-  const startTime = endTime - yearInMiliSec;
+  let endTime = Math.floor(Date.now() / 1000);
+  const yearInSec = 365 * 24 * 60 * 60;
+  const startTime = endTime - yearInSec;
   const requestCount = 5;
 
-  const storagePath = "C:/Users/AURES/Documents/storage.json";
-  const storageObj = JSON.parse(fs.readFileSync(storagePath, "utf-8"));
+  const storageObj = JSON.parse(fs.readFileSync(storagePath[1], "utf-8"));
 
   while (endTime > startTime) {
-    try {
+    try { 
       const params = {
         endTime: Math.floor(endTime),
         startTime: Math.floor(startTime),
@@ -34,18 +45,17 @@ export async function fetchLastYearMatchIds(puuid, region) {
       );
       //save matchIds
       storageObj.yearMatchIDs.push(...matchIDs);
-      console.log("here?");
-      fs.writeFileSync(storagePath, JSON.stringify(storageObj, null, 2));
-      console.log("storage ready?");
-      endTime = await getMatchTimeStamp(matchIDs[0],region);
-      console.log("new endTime: " + endTime);
-      console.log("new startTime: " + startTime);
+      endTime = await getMatchTimeStamp(matchIDs[matchIDs.length - 1],region) - 1;
+      fs.writeFileSync(storagePath[1], JSON.stringify(storageObj, null, 2));
       await new Promise((res) => setTimeout(res, 1000));
     } catch (error) {
       console.error("Error fetching matches:", error.message);
       await new Promise((res) => setTimeout(res, 5000));
       requestCount-=1;
-      if(requestCount == 0 ) break;
+      if(requestCount == 0 ) {
+        return true;
+      }
     }
   }
+  return true;
 }
